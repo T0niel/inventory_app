105065e3679620b1e887947beb6250ceec46f2d9
Elton Rexha <elton.rexha123@outlook.com>
2024-11-05 20:30:31 +0100

    add categories partial and style it

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const { renderIndex } = require('../controllers/indexController');
 const router = Router();
 
+router.get('/:category', renderIndex);
 router.get('/', renderIndex);
 
 module.exports = router;
