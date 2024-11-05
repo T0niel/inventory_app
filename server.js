4d6ca2f2190c5495fe655052b9971314eb25903b
Elton Rexha <elton.rexha123@outlook.com>
2024-11-05 19:18:10 +0100

    add controller, routes and views files

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 
 const express = require('express');
-const app = express();
 const path = require('path');
+const indexRouter = require('./routes/indexRouter');
+const indexRouter = require('./routes/inventoryRouter');
+
+const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use('/', indexRouter);
+app.use('/inventory', inventoryRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
